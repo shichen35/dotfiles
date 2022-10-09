5bdb9db959b5dc81fa0e994b1bc91a37f9a18093
shichen35 <shichen35@gmail.com>
2022-10-08 18:58:08 -0700

    add after plugin scripts for rust, go, and c/c++

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,2 @@
-require("chase")
+require("core")
 
--- nvim/.config/nvim/lua/chase/init.lua
+++
@@ -1,7 +0,0 @@
-if require('chase.install')() then
-  return
-end
-require("chase.packer")
-require("chase.set")
-require("chase.plug")
-require("chase.keymap")
