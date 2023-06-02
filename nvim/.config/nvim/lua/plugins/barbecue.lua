a3dd690e161003e33bd62676fc810a89ebde95ef
Chen Shi <shichen35@gmail.com>
2023-06-02 00:16:25 -0700

    update nvimtree config

--- nvim/.config/nvim/lua/plugins/barbecue.lua
+++ nvim/.config/nvim/lua/plugins/barbecue.lua
@@ -1,13 +1,13 @@
 return {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-      -- configurations go here
-    },
-  }
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",   -- optional dependency
+  },
+  opts = {
+    -- configurations go here
+  },
+}
