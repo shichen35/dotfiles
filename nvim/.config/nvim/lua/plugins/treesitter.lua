0745cf2f215c0f59f307afb9db12cc546f89fd94
Chen Shi <shichen35@gmail.com>
2023-06-02 19:44:12 -0700

    add trouble

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -17,13 +17,29 @@ function M.config()
   local configs = require "nvim-treesitter.configs"
 
   configs.setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "rust", "c", "cpp" }, -- put the language you want in this array
+    ensure_installed = {
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "html",
+      "javascript",
+      "typescript",
+      "tsx",
+      "javascript",
+      "json",
+      "yaml",
+      "python",
+      "rust",
+      "c",
+      "cpp",
+    }, -- put the language you want in this array
     -- ensure_installed = "all", -- one of "all" or a list of languages
-    ignore_install = { "" },                                                       -- List of parsers to ignore installing
-    sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
+    ignore_install = { "" }, -- List of parsers to ignore installing
+    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 
     highlight = {
-      enable = true,       -- false will disable the whole extension
+      enable = true, -- false will disable the whole extension
       disable = { "css" }, -- list of language that will be disabled
     },
     autopairs = {
