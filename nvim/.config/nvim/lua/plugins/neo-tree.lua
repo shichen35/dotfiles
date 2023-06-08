659411b0bcb763a264f3d1dffcb7edd8abd64bd0
shichen35 <shichen35@gmail.com>
2023-06-08 16:15:32 -0700

    add plugin config

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -10,4 +10,14 @@ return {
   keys = {
     { "<leader>e", "<cmd>NeoTreeFocusToggle<CR>", desc = "Toggle NeoTree Explorer" },
   },
+  event = "VimEnter",
+  opts = {
+    close_if_last_window = true,              -- Close Neo-tree if it is the last window left in the tab
+    filesystem = {
+      follow_current_file = true,             -- This will find and focus the file in the active buffer every
+      -- time the current file is changed while the tree is open.
+      group_empty_dirs = true,                -- when true, empty folders will be grouped together
+      hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
+    },
+  },
 }
