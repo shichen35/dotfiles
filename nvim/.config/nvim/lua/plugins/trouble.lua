c6b8970714cd7c2b7f8dde81387d37e73430bacc
Chen Shi <shichen35@gmail.com>
2023-06-13 16:18:12 -0700

    add nvim bqf

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,20 +1,21 @@
 -- better diagnostics list and others
-  return {
+return {
+  {
     "folke/trouble.nvim",
     cmd = { "TroubleToggle", "Trouble" },
     opts = { use_diagnostic_signs = true },
     keys = {
-      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics (Trouble)" },
       { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-      { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-      { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+      { "<leader>xL", "<cmd>TroubleToggle loclist<cr>",               desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>",              desc = "Quickfix List (Trouble)" },
       {
         "[q",
         function()
           if require("trouble").is_open() then
-            require("trouble").previous({ skip_groups = true, jump = true })
+            require("trouble").previous { skip_groups = true, jump = true }
           else
-            vim.cmd.cprev()
+            pcall(vim.cmd, "cprev")
           end
         end,
         desc = "Previous trouble/quickfix item",
@@ -23,12 +24,15 @@
         "]q",
         function()
           if require("trouble").is_open() then
-            require("trouble").next({ skip_groups = true, jump = true })
+            require("trouble").next { skip_groups = true, jump = true }
           else
-            vim.cmd.cnext()
+            pcall(vim.cmd, "cnext")
           end
         end,
         desc = "Next trouble/quickfix item",
       },
+      { "gR", "<cmd>TroubleToggle lsp_references<cr>", desc = "References List (Trouble)" },
     },
-  }
+  },
+  { "kevinhwang91/nvim-bqf", ft = "qf" },
+}
