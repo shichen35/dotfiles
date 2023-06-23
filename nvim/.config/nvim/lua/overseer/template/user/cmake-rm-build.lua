ab923b9a5d5cc5b97dede588a0943ced956c8f86
shichen35 <shichen35@gmail.com>
2023-06-22 22:48:18 -0700

    add overseer plugin

---
+++ nvim/.config/nvim/lua/overseer/template/user/cmake-rm-build.lua
@@ -0,0 +1,24 @@
+return {
+  name = "cmake rm build",
+  builder = function()
+    return {
+      cmd = { "rm" },
+      args = { "-rf", "build" },
+    }
+  end,
+}
+-- {
+--   name = "cmake build debug",
+--   builder = function()
+--     return {
+--       cmd = { "cmake" },
+--       args = {
+--         ".",
+--         "-B=build",
+--         '-G="Unix Makefiles"',
+--         "-DCMAKE_BUILD_TYPE=Debug",
+--         "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
+--       },
+--     }
+--   end,
+-- },
