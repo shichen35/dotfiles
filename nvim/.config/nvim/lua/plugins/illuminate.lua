87a3def631b58ef37b1c8ef33e32c795f8a7b586
Chen Shi <shichen35@gmail.com>
2023-07-03 21:54:37 -0700

    update autogroup

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -4,15 +4,15 @@ local M = {
 }
 
 M.opts = {
-  modes_denylist = { "v", "vs", "V", "Vs", "", "s" },
+  -- modes_denylist = { "v", "vs", "V", "Vs", "", "s" },
   filetypes_denylist = { "alpha" },
 }
 
 function M.config(_, opts)
   require("illuminate").configure(opts)
-  vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-  vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-  vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+  -- vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+  -- vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+  -- vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
 end
 
 return M
