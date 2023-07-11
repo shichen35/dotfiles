b3fc983e18694c627e0e91f3f0de10e8a2780313
shichen35 <shichen35@gmail.com>
2023-07-10 20:26:22 -0700

    add alpha term

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -7,8 +7,33 @@ local M = {
 }
 
 function M.config()
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'AlphaReady',
+    desc = 'hide cursor for alpha',
+    callback = function()
+      -- local hl = vim.api.nvim_get_hl_by_name('Cursor', true)
+      -- hl.blend = 100
+      -- vim.api.nvim_set_hl(0, 'Cursor', hl)
+      -- vim.opt.guicursor:append('a:Cursor/lCursor')
+      vim.opt.cursorline = true
+    end,
+  })
+  vim.api.nvim_create_autocmd('BufUnload', {
+    buffer = 0,
+    desc = 'show cursor after alpha',
+    callback = function()
+      -- local hl = vim.api.nvim_get_hl_by_name('Cursor', true)
+      -- hl.blend = 0
+      -- vim.api.nvim_set_hl(0, 'Cursor', hl)
+      -- vim.opt.guicursor:remove('a:Cursor/lCursor')
+      vim.opt.cursorline = false
+    end,
+  })
+
   local alpha = require "alpha"
   local dashboard = require "alpha.themes.dashboard"
+  -- local alphaterm = require "alpha.term"
+
   dashboard.section.header.val = {
     [[                               __                ]],
     [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -19,10 +44,10 @@ function M.config()
   }
   dashboard.section.buttons.val = {
     dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert<CR>"),
+    dashboard.button("p", " " .. " Projects", ":lua require('telescope').extensions.projects.projects()<CR>"),
     dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles<CR>"),
-    dashboard.button("f", " " .. " Find file", ":Telescope find_files<CR>"),
-    dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-    dashboard.button("g", " " .. " Find text", ":Telescope live_grep<CR>"),
+    -- dashboard.button("f", " " .. " Find file", ":Telescope find_files<CR>"),
+    -- dashboard.button("g", " " .. " Find text", ":Telescope live_grep<CR>"),
     dashboard.button("c", " " .. " Config", ":e $MYVIMRC<CR>"),
     dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
     dashboard.button("q", " " .. " Quit", ":qa<CR>"),
@@ -38,6 +63,25 @@ function M.config()
   dashboard.section.buttons.opts.hl = "Keyword"
 
   dashboard.opts.opts.noautocmd = true
+
+  -- local width = 46
+  -- local height = 25 -- two pixels per vertical space
+  -- dashboard.section.terminal.command = "cat | " .. os.getenv("HOME") .. "/.dotfiles/art/thisisfine.sh"
+  -- dashboard.section.terminal.width = width
+  -- dashboard.section.terminal.height = height
+  -- dashboard.section.terminal.opts.redraw = true
+
+  -- dashboard.config.layout = {
+  --   { type = "padding", val = 1 },
+  --   dashboard.section.terminal,
+  --   { type = "padding", val = 1 },
+  --   dashboard.section.header,
+  --   { type = "padding", val = 2 },
+  --   dashboard.section.buttons,
+  --   { type = "padding", val = 1 },
+  --   dashboard.section.footer,
+  -- }
+
   alpha.setup(dashboard.opts)
 
   vim.api.nvim_create_autocmd("User", {
