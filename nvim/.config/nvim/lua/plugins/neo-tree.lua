4a892227e776f64b009bda0da7e2f4ba67b5968d
Chen Shi <shichen35@gmail.com>
2023-10-23 13:03:33 -0700

    change neotree to autoclosing panel

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -11,12 +11,21 @@ return {
     { "<leader>e", "<cmd>NeoTreeFocusToggle<CR>", desc = "Toggle NeoTree Explorer" },
   },
   opts = {
-    close_if_last_window = true,              -- Close Neo-tree if it is the last window left in the tab
+    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
     filesystem = {
-      follow_current_file = true,             -- This will find and focus the file in the active buffer every
+      follow_current_file = true, -- This will find and focus the file in the active buffer every
       -- time the current file is changed while the tree is open.
-      group_empty_dirs = true,                -- when true, empty folders will be grouped together
+      group_empty_dirs = true, -- when true, empty folders will be grouped together
       hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+    },
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          -- OR
+          require("neo-tree.command").execute { action = "close" }
+        end,
+      },
     },
   },
 }
