5af5c68ca00bf42d2fb386d1c8d577b921205f7e
shichen35 <shichen35@gmail.com>
2023-11-13 19:09:44 -0800

    update conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,27 +1,32 @@
 return {
-  'stevearc/conform.nvim',
-  event = { 'BufReadPost', 'BufNewFile' },
+  "stevearc/conform.nvim",
+  event = { "BufReadPost", "BufNewFile" },
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
       -- Conform will run multiple formatters sequentially
       python = { "isort", "black" },
       -- Use a sub-list to run only the first available formatter
-      javascript = { 'eslint_d', 'prettier' },
-      javascriptreact = { 'eslint_d', 'prettier' },
-      typescript = { 'eslint_d', 'prettier' },
-      typescriptreact = { 'eslint_d', 'prettier' },
-      css = { { 'prettierd', 'prettier' } },
-      scss = { { 'prettierd', 'prettier' } },
-      less = { { 'prettierd', 'prettier' } },
-      html = { { 'prettierd', 'prettier' } },
-      json = { { 'prettierd', 'prettier' } },
-      markdown = { { 'prettierd', 'prettier' } },
+      javascript = { "eslint_d", "prettier" },
+      javascriptreact = { "eslint_d", "prettier" },
+      typescript = { "eslint_d", "prettier" },
+      typescriptreact = { "eslint_d", "prettier" },
+      css = { { "prettierd", "prettier" } },
+      scss = { { "prettierd", "prettier" } },
+      less = { { "prettierd", "prettier" } },
+      html = { { "prettierd", "prettier" } },
+      json = { { "prettierd", "prettier" } },
+      markdown = { { "prettierd", "prettier" } },
       -- Use the "*" filetype to run formatters on all filetypes.
       ["*"] = { "codespell" },
       -- Use the "_" filetype to run formatters on filetypes that don't
       -- have other formatters configured.
       ["_"] = { "trim_whitespace" },
+    },
+    formatters = {
+      prettier = {
+        prepend_args = { "--no-semi", "--single-quote" }, -- --jsx-single-quote
+      },
     },
   },
 }
