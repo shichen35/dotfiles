c488c0c051dd7a17608f8bac96f01c8a121af7e9
Chen Shi <shichen35@gmail.com>
2023-12-15 17:06:00 -0800

    add vim-visual-multi

---
+++ nvim/.config/nvim/lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,30 @@
+return {
+  'mg979/vim-visual-multi',
+      event = "VeryLazy",
+  -- keys = {
+  --   { 'C-n', nil, mode = 'n' },
+  --   { 'C-Down', mode = 'n' },
+  --   { 'C-Up', mode = 'n' },
+  --   { '<leader>mp', mode = 'n' },
+  --   { '<leader>ma', mode = 'n' },
+  --   { '<leader>ma', mode = 'v' },
+  -- },
+  init = function()
+    vim.g.VM_default_mappings = 0
+    vim.g.VM_maps = {
+      ['Find Under'] = '<C-n>',
+      ['Find Subword Under'] = '<C-n>',
+      ['Goto Next'] = 'n',
+      ['Goto Prev'] = 'N',
+      ['Skip Region'] = 'q',
+      ['Remove Region'] = 'Q',
+      ['Add Cursor Down'] = '<C-Down>',
+      ['Add Cursor Up'] = '<C-Up>',
+      ['Add Cursor At Pos'] = '<leader>mp',
+      ['Select All'] = '<leader>ma',
+      ['Visual All'] = '<leader>ma',
+      -- ["Start Regex Search"] = "<C-/>",
+      ['Exit'] = '<Esc>',
+    }
+  end,
+}
