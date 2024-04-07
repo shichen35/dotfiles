b1dc195e72ed9f01249d0722c5696e9b70e8307c
Chen Shi <shichen35@gmail.com>
2024-04-07 13:30:29 -0700

    add macos drag anywhere feature

--- zsh-files/mac.zsh
+++ zsh-files/mac.zsh
@@ -8,6 +8,9 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 20
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+# drag window from anywhere with ctrl + cmd + click
+defaults write -g NSWindowShouldDragOnGesture -bool true
+
 export ANSIBLE_VAULT_PASSWORD_FILE=/Users/chen.shi/Developer/ansible-vault.pass
 
 alias wpm='wpm --tag chen'
