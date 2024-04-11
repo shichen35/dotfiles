d03e1eb4a6eb35fbde5dbd4689d0f38e70a3dd8f
Chen Shi <shichen35@gmail.com>
2024-04-11 11:11:00 -0700

    remove gruvbox and tokyonight

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,44 +1,17 @@
--- local M = {
---   {
---     'catppuccin/nvim',
---     name = 'catppuccin',
---     lazy = false,
---     priority = 1000,
---     dependencies = {
---       'folke/tokyonight.nvim',
---       {
---         'sainnhe/gruvbox-material',
---         init = function()
---           vim.g.gruvbox_material_better_performance = 1
---           vim.g.gruvbox_material_foreground = 'mix'
---           vim.g.gruvbox_material_background = 'hard'
---         end,
---       },
---     },
---     config = function()
---       -- load the colorscheme here
---       vim.cmd.colorscheme 'gruvbox-material'
---     end,
---   },
--- }
--- return M
-
 return {
-  "ellisonleao/gruvbox.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
-  dependencies = {
-    {
-      "catppuccin/nvim",
-      name = "catppuccin",
-    },
-    "folke/tokyonight.nvim",
-  },
-  init = function()
-    vim.g.gruvbox_material_better_performance = 1
-    vim.g.gruvbox_material_foreground = "mix"
-    vim.g.gruvbox_material_background = "hard"
-  end,
+  -- dependencies = {
+  --   "ellisonleao/gruvbox.nvim",
+  --   "folke/tokyonight.nvim",
+  -- },
+  -- init = function()
+  --   vim.g.gruvbox_material_better_performance = 1
+  --   vim.g.gruvbox_material_foreground = "mix"
+  --   vim.g.gruvbox_material_background = "hard"
+  -- end,
   config = function()
     -- load the colorscheme here
     vim.cmd.colorscheme("catppuccin-macchiato")
