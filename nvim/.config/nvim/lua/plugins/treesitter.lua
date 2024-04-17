d3f31d5de86cfdd140d0fc654a1661e93aba506f
Chen Shi <shichen35@gmail.com>
2024-04-16 22:47:45 -0700

    treesitter update

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,11 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "BufReadPost",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    build = ":TSUpdate",
     dependencies = {
       {
         "JoosepAlviste/nvim-ts-context-commentstring",
