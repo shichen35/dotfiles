b85b9abbc4765d2a2f8a50a7ab128668733a1289
Chen Shi <shichen35@gmail.com>
2024-08-13 12:55:20 -0700

    feat: add alacritty theme, fix: which-key warning

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -4,23 +4,16 @@ local M = {
   config = function(_, opts)
     local wk = require("which-key")
     wk.setup(opts)
-    wk.register({
+    wk.add({
       mode = { "n", "v" },
-      -- ["g"] = { name = "+goto" },
-      -- ["gs"] = { name = "+surround" },
-      -- ["]"] = { name = "+next" },
-      -- ["["] = { name = "+prev" },
-      ["<leader><tab>"] = { name = "+tabs" },
-      ["<leader>d"] = { name = "+debug" },
-      ["<leader>l"] = { name = "+lsp" },
-      ["<leader>f"] = { name = "+file/find" },
-      ["<leader>g"] = { name = "+git" },
-      -- ["<leader>q"] = { name = "+quit/session" },
-      ["<leader>s"] = { name = "+search" },
-      ["<leader>t"] = { name = "+task" },
-      -- ["<leader>u"] = { name = "+ui" },
-      -- ["<leader>w"] = { name = "+windows" },
-      ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      { "<leader><tab>", group = "tabs" },
+      { "<leader>d", group = "debug" },
+      { "<leader>f", group = "file/find" },
+      { "<leader>g", group = "git" },
+      { "<leader>l", group = "lsp" },
+      { "<leader>s", group = "search" },
+      { "<leader>t", group = "task" },
+      { "<leader>x", group = "diagnostics/quickfix" },
     })
   end,
 }
