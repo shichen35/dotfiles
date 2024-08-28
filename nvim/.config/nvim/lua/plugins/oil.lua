a8c61c99d30e7dbfbb7b62de3ff7e00a0425fe2a
shichen35 <shichen35@gmail.com>
2024-08-28 11:29:33 -0700

    feat: replace nvim-tree with oil.nvim

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/oil.nvim",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("oil").toggle_float()
+      end,
+      mode = "n",
+      desc = "Oil File Explorer",
+    },
+  },
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function()
+    require("oil").setup({})
+  end,
+}
