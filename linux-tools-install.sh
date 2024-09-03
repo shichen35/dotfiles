83c7683e7566422702f1b0546b0a71ed20b70323
shichen35 <shichen35@gmail.com>
2024-09-03 10:26:14 -0700

    feat: remove bottom from linux tools

--- linux-tools-install.sh
+++ linux-tools-install.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/zsh
 cargo install cargo-update \
-atuin \
-tealdeer \
-fd-find \
-ripgrep \
-eza \
-joshuto \
-dua-cli \
-onefetch \
-macchina \
-zoxide \
-tokei \
-bottom
-
+    atuin \
+    tealdeer \
+    fd-find \
+    ripgrep \
+    eza \
+    joshuto \
+    dua-cli \
+    onefetch \
+    macchina \
+    zoxide \
+    tokei
 
 # brew install tealdeer fd ripgrep joshuto macchina zoxide tokei dua-cli atuin
--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,107 +1,108 @@
-return {
-  -- "nvim-tree/nvim-tree.lua",
-  -- version = "*",
-  -- lazy = false,
-  -- keys = {
-  --   { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "NvimTree Explorer" },
-  --   {
-  --     "<leader>fe",
-  --     function()
-  --       require("nvim-tree.api").tree.find_file({
-  --         update_root = true,
-  --         open = true,
-  --         focus = true,
-  --       })
-  --     end,
-  --     desc = "Nvim Tree Find File",
-  --   },
-  -- },
-  -- dependencies = {
-  --   "nvim-tree/nvim-web-devicons",
-  -- },
-  -- config = function()
-  --   require("nvim-tree").setup({
-  --     -- temporary fix for the floating window: https://github.com/nvim-tree/nvim-tree.lua/issues/2749
-  --     hijack_netrw = false,
-  --     sync_root_with_cwd = true,
-  --     view = {
-  --       -- width = 35,
-  --       -- relativenumber = true,
-  --       -- side = "right",
-  --       float = {
-  --         enable = true,
-  --         open_win_config = {
-  --           relative = "editor",
-  --           border = "rounded",
-  --           width = 40,
-  --           height = 22,
-  --           row = 0,
-  --           col = 0,
-  --         },
-  --       },
-  --     },
-  --     filters = {
-  --       custom = { "^.git$" },
-  --     },
-  --     git = { enable = true, ignore = true },
-  --     update_focused_file = {
-  --       enable = false,
-  --       update_root = true,
-  --       ignore_list = { "help" },
-  --     },
-  --     diagnostics = {
-  --       enable = true,
-  --       icons = {
-  --         hint = "",
-  --         info = "",
-  --         warning = "",
-  --         error = "",
-  --       },
-  --     },
-  --     modified = {
-  --       enable = true,
-  --     },
-  --     actions = {
-  --       change_dir = {
-  --         global = true,
-  --       },
-  --       open_file = {
-  --         resize_window = true,
-  --         quit_on_open = true,
-  --       },
-  --       file_popup = {
-  --         open_win_config = {
-  --           border = vim.g.FloatBorders,
-  --         },
-  --       },
-  --     },
-  --
-  --     renderer = {
-  --       highlight_opened_files = "name",
-  --       icons = {
-  --         git_placement = "before",
-  --         glyphs = {
-  --           --     ignored = "◌",
-  --           --   },
-  --           git = {
-  --             unstaged = "",
-  --             staged = "",
-  --             unmerged = "",
-  --             renamed = "󰛿",
-  --             untracked = "",
-  --             deleted = "",
-  --             ignored = "◌",
-  --           },
-  --         },
-  --       },
-  --     },
-  --     filesystem_watchers = {
-  --       enable = true,
-  --       debounce_delay = 50,
-  --       ignore_dirs = {
-  --         "node_modules",
-  --       },
-  --     },
-  --   })
-  -- end,
-}
+return {}
+-- return {
+--   "nvim-tree/nvim-tree.lua",
+--   version = "*",
+--   lazy = false,
+--   keys = {
+--     { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "NvimTree Explorer" },
+--     {
+--       "<leader>fe",
+--       function()
+--         require("nvim-tree.api").tree.find_file({
+--           update_root = true,
+--           open = true,
+--           focus = true,
+--         })
+--       end,
+--       desc = "Nvim Tree Find File",
+--     },
+--   },
+--   dependencies = {
+--     "nvim-tree/nvim-web-devicons",
+--   },
+--   config = function()
+--     require("nvim-tree").setup({
+--       -- temporary fix for the floating window: https://github.com/nvim-tree/nvim-tree.lua/issues/2749
+--       hijack_netrw = false,
+--       sync_root_with_cwd = true,
+--       view = {
+--         -- width = 35,
+--         -- relativenumber = true,
+--         -- side = "right",
+--         float = {
+--           enable = true,
+--           open_win_config = {
+--             relative = "editor",
+--             border = "rounded",
+--             width = 40,
+--             height = 22,
+--             row = 0,
+--             col = 0,
+--           },
+--         },
+--       },
+--       filters = {
+--         custom = { "^.git$" },
+--       },
+--       git = { enable = true, ignore = true },
+--       update_focused_file = {
+--         enable = false,
+--         update_root = true,
+--         ignore_list = { "help" },
+--       },
+--       diagnostics = {
+--         enable = true,
+--         icons = {
+--           hint = "",
+--           info = "",
+--           warning = "",
+--           error = "",
+--         },
+--       },
+--       modified = {
+--         enable = true,
+--       },
+--       actions = {
+--         change_dir = {
+--           global = true,
+--         },
+--         open_file = {
+--           resize_window = true,
+--           quit_on_open = true,
+--         },
+--         file_popup = {
+--           open_win_config = {
+--             border = vim.g.FloatBorders,
+--           },
+--         },
+--       },
+--
+--       renderer = {
+--         highlight_opened_files = "name",
+--         icons = {
+--           git_placement = "before",
+--           glyphs = {
+--             --     ignored = "◌",
+--             --   },
+--             git = {
+--               unstaged = "",
+--               staged = "",
+--               unmerged = "",
+--               renamed = "󰛿",
+--               untracked = "",
+--               deleted = "",
+--               ignored = "◌",
+--             },
+--           },
+--         },
+--       },
+--       filesystem_watchers = {
+--         enable = true,
+--         debounce_delay = 50,
+--         ignore_dirs = {
+--           "node_modules",
+--         },
+--       },
+--     })
+--   end,
+-- }
