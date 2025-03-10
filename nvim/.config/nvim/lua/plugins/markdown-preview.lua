f4d46db9276030439bd73ef0c4ec6f4e822edb96
Chen Shi <shichen35@gmail.com>
2025-03-10 10:36:38 -0700

    feat: minor changes

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,7 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  ft = "markdown",
+  -- "iamcco/markdown-preview.nvim",
+  -- build = function()
+  --   vim.fn["mkdp#util#install"]()
+  -- end,
+  -- ft = "markdown",
 }
