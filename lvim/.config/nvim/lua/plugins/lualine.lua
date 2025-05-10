b05acc2a7da9b81784ba39bc41e2189b9694968f
Chen Shi <shichen35@gmail.com>
2025-05-10 15:16:23 -0700

    feat: customize lvim

---
+++ lvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,72 @@
+local spaces = function()
+  return "spaces: " .. vim.api.nvim_get_option_value("shiftwidth", {})
+end
+
+return {
+  "nvim-lualine/lualine.nvim",
+
+  opts = function(_, opts)
+    local icons = LazyVim.config.icons
+    opts.options = vim.tbl_deep_extend("force", opts.options or {}, {
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    })
+
+    opts.sections = vim.tbl_deep_extend("force", opts.sections or {}, {
+      lualine_c = {
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+      },
+      lualine_x = {
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = function() return { fg = Snacks.util.color("Debug") } end,
+        },
+        -- stylua: ignore
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = function()
+            return { fg = Snacks.util.color("Special") }
+          end,
+        },
+        {
+          "diff",
+          symbols = {
+            added = icons.git.added,
+            modified = icons.git.modified,
+            removed = icons.git.removed,
+          },
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
+            end
+          end,
+        },
+        { "encoding" },
+        {
+          "filetype",
+          icons_enabled = true,
+        },
+      },
+      lualine_y = {
+        { "location", padding = 0 },
+      },
+      lualine_z = { "progress" },
+    })
+  end,
+}
--- lvim/.config/nvim/lua/plugins/snacks.lua
+++ lvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,39 @@
+vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+-- stylua: ignore
+-- local local_header = [[
+--  ██████   █████                   █████   █████  ███                 
+-- ░░██████ ░░███                   ░░███   ░░███  ░░░                  
+--  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████  
+--  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███ 
+--  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███ 
+--  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███ 
+--  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████
+-- ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░ 
+-- ]]
+
+-- stylua: ignore
+-- local local_header = [[
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+-- ]]
+
+-- stylua: ignore
+local local_header = [[
+           ▄ ▄                   
+       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     
+       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     
+    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     
+  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  
+  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄
+▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █
+█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █
+    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    
+]]
+
 return {
   "snacks.nvim",
   opts = {
@@ -6,27 +42,34 @@ return {
         pick = function(cmd, opts)
           return LazyVim.pick(cmd, opts)()
         end,
-        header = [[
-                               __                
-  ___     ___    ___   __  __ /\_\    ___ ___    
- / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  
-/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ 
-\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\
- \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/
- ]],
+        --         header = [[
+        --                                __
+        --   ___     ___    ___   __  __ /\_\    ___ ___
+        --  / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\
+        -- /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \
+        -- \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\
+        --  \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/
+        --  ]],
+
+        header = { local_header, align = "center", hl = "special" },
         -- stylua: ignore
         ---@type snacks.dashboard.Item[]
         keys = {
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           -- { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
           -- { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
       },
     },
   },
