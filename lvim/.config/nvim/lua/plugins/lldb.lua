e3914facd4108284468512ab340055b32947aff4
Chen Shi <shichen35@gmail.com>
2025-05-11 14:49:02 -0700

    feat: configure rust dap

---
+++ lvim/.config/nvim/lua/plugins/lldb.lua
@@ -0,0 +1,29 @@
+return {
+  "mfussenegger/nvim-dap",
+  opts = {
+    setup = {
+      codelldb = function()
+        local dap = require("dap")
+        local mason_registry = require("mason-registry")
+        local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/"
+        local codelldb_path = codelldb_root .. "codelldb"
+
+        dap.adapters.codelldb = {
+          type = "server",
+          port = "${port}",
+          executable = {
+            command = codelldb_path,
+            args = { "--port", "${port}" },
+            -- On windows you may have to uncomment this:
+            -- detached = false,
+          },
+        }
+
+        dap.adapters.c = dap.adapters.codelldb
+        dap.adapters.cpp = dap.adapters.codelldb
+        dap.adapters.cppdbg = dap.adapters.codelldb
+        dap.adapters.rust = dap.adapters.codelldb
+      end,
+    },
+  },
+}
