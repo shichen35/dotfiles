34f0af3ff3aeea5ac161fc758ab56b06346d01dd
Chen Shi <shichen35@gmail.com>
2026-08-29 17:53:26 -0700

    feat: comment out fcitx config

--- zsh-files/work.zsh
+++ zsh-files/work.zsh
@@ -1,7 +1,7 @@
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-export DefaultIMModule=fcitx
+# export GTK_IM_MODULE=fcitx
+# export QT_IM_MODULE=fcitx
+# export XMODIFIERS=@im=fcitx
+# export DefaultIMModule=fcitx
 
 typeset -U path PATH
 
